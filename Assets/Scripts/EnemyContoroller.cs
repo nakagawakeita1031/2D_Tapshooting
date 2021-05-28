9464ce332ce523e92f2118100bb9fa9df9d78320
Nakagawakeita <nakagawakeita10@gmail.com>
2021-05-28 13:33:16 +0900

    手順9～10終了 (手順9)localposition (手順10)引数の受け渡し

---
+++ Assets/Scripts/EnemyContoroller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyContoroller : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //このスクリプトがアタッチされているゲームオブジェクトを徐々に移動させる
+        transform.Translate(0, moveSpeed, 0);
+
+        //Enemyの位置が一定値を超えたら
+        if (transform.localPosition.y < -1500f)
+        {
+            
+            Destroy(gameObject);
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            DestroyObjects(collision);
+        }
+       
+    }
+
+    /// <summary>
+    /// バレットとエネミーの破壊処理
+    /// </summary>
+    /// <param name="collision"></param>
+    private void DestroyObjects(Collider2D collision)
+    {
+        //侵入判定確認
+        Debug.Log("侵入したオブジェクト名：" + collision.gameObject.tag);
+
+        //Bulletゲームオブジェクトを破壊
+        Destroy(collision.gameObject);
+
+        //Enemy破壊
+        Destroy(this.gameObject);
+    }
+}
