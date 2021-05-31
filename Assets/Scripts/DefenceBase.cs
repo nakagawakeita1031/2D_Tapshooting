681bf5cef992ccaac8bdb1175f8af07cc1670fab
Nakagawakeita <nakagawakeita10@gmail.com>
2021-06-01 08:33:07 +0900

    手順１２終了

---
+++ Assets/Scripts/DefenceBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DefenceBase : MonoBehaviour
+{
+    [Header("拠点の耐久値")]
+    public int durability;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //侵入判定処理
+        if (collision.gameObject.tag == "Enemy")
+        {
+            durability -= 10;
+
+            Debug.Log("残りの耐久値：" + durability);
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+}
