9b4cd98ccf60f12be63f3043dbe05d244886d96b
Nakagawakeita <nakagawakeita10@gmail.com>
2021-06-03 11:27:11 +0900

    手順１５終了

--- Assets/Scripts/DefenceBase.cs
+++ Assets/Scripts/DefenceBase.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class DefenceBase : MonoBehaviour
 {
     [Header("拠点の耐久値")]
     public int durability;
+
+    [SerializeField]
+    private Text txtDurability;
+
+    private int minDurability = 0; //耐久値の最小を代入
+    private int maxDurability; //耐久値の最大を代入
+
+    void Start()
+    {
+        //ゲームの開始時点の耐久値を最大値として代入する
+        maxDurability = durability;
+
+        DisplayDurability();
+
+        //TODO ゲージの表示を耐久値に合わせて更新する
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,12 +45,28 @@ public class DefenceBase : MonoBehaviour
     {
         durability -= enemyContoroller.enemyPower;
 
+        //耐久値を上限・下限値の範囲内に収まるかを確認し、それを超えた場合には上限・制限する
+        durability = Mathf.Clamp(durability, minDurability, maxDurability);
+
         Debug.Log("残りの耐久値" + durability);
+
+        DisplayDurability();
+
+
+        //TODO ゲージの表示を耐久値に合わせて更新
 
         //TODO 耐久値が0以下になっていないか確認
 
         //TODO 耐久値が0以下なら、ゲームオーバーの判定を行う
     }
 
+    /// <summary>
+    /// 耐久値の表示更新
+    /// </summary>
+    private void DisplayDurability()
+    {
+        //画面に耐久値を現在 / 最大値の形式で表示する
+        txtDurability.text = durability + "/" + maxDurability;
+    }
 
 }
