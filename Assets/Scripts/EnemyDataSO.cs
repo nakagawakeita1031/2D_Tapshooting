7e8efa00dfe01935109687647c75698c9440cd30
Nakagawakeita <nakagawakeita10@gmail.com>
2021-08-03 00:17:17 +0900

    発展2　 ・スクリタブル・オブジェクトの作成とデータの登録 ・[System.Serializable(シリアライザブル)]属性 ・Listの初期化

---
+++ Assets/Scripts/EnemyDataSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "EnemyDataSO", menuName = "Create EnemyDataSO")]
+public class EnemyDataSO : ScriptableObject
+{
+    public List<EnemyData> enemyDataList = new List<EnemyData>();
+
+    [Serializable]
+    public class EnemyData
+    {
+        public int no;                //エネミーの通し番号
+        public int hp;　　　　　　　　//エネミーのHp
+        public int power;　　　　　　 //エネミーの攻撃力
+        public Sprite enemySprite;　　//エネミーの画像
+        public EnemyType enemyType;　 //エネミーのタイプ
+    }
+}
